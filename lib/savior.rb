725f23fbdb5787b29268643c08d701dbd360bbcf
Devi <dfedogawa3@gmail.com>
2021-06-05 01:17:45 +0700

    first commit

---
+++ lib/savior.rb
@@ -0,0 +1,9 @@
+require "savior/version"
+require "savior/average"
+require "savior/person_die"
+require "savior/person"
+
+module Savior
+  class Error < StandardError; end
+
+end
---
+++ lib/savior/version.rb
@@ -0,0 +1,3 @@
+module Savior
+  VERSION = "0.0.1"
+end
